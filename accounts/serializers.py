5aaaaa5751778f07235ad5425d82e961790acd56
Rafi Louliki <rafilouliki98@gmail.com>
2023-09-14 21:52:07 +0300

    Uploading files

---
+++ accounts/serializers.py
@@ -0,0 +1,10 @@
+from djoser.serializers import UserCreateSerializer
+from accounts.models import User
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model=User
+        fields=('id', 'email', 'name', 'password')
+
+
+
